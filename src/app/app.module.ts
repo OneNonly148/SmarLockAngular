fbdac9712c46d96b29c50fb0ebd46a7635f02cfe
Wan Akashah <wanakashah14833@gmail.com>
2018-07-23 11:40:40 +0800

    Init SmartLock Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
     BrowserModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB7yhJtk0ino5-hzZILOfx4fVEsws4qzPU",
+    authDomain: "smartlock-fc432.firebaseapp.com",
+    databaseURL: "https://smartlock-fc432.firebaseio.com",
+    projectId: "smartlock-fc432",
+    storageBucket: "smartlock-fc432.appspot.com",
+    messagingSenderId: "813331204757"
+  }
 };
