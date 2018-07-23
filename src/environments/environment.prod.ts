0bd3addae594a12d5db0258134f2909d502beb3b
Wan Akashah <wanakashah14833@gmail.com>
2018-07-23 13:00:23 +0800

    Deploy to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB7yhJtk0ino5-hzZILOfx4fVEsws4qzPU",
+    authDomain: "smartlock-fc432.firebaseapp.com",
+    databaseURL: "https://smartlock-fc432.firebaseio.com",
+    projectId: "smartlock-fc432",
+    storageBucket: "smartlock-fc432.appspot.com",
+    messagingSenderId: "813331204757"
+  }
 };
